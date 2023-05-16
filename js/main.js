8a8c98ddae37f94aa237c223e8d6cf274ead0421
morodola <armael92@mail.ru>
2023-05-16 13:19:47 +0300

    slider end

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+   loop: true,
+    
+  keyboard: {
+  enabled: true,
+  onlyInViewport: false,
   },
+ 
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
   },
 });
+
+ 
