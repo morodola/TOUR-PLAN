2d6189e6b12c76ab56f1221888a98576a46ce737
morodola <armael92@mail.ru>
2023-06-02 02:37:31 +0300

    fix menu mobile

--- js/main.js
+++ js/main.js
@@ -32,5 +32,10 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-
- 
+var menuButton = document.querySelector('.menu-button');
+ menuButton.addEventListener('click',function(){
+  console.log('клик по кнопке меню');
+  document
+    .querySelector('.navbar-bottom')
+    .classList.toggle('navbar-bottom--visible');
+ })
